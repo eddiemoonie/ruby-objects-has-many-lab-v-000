5b267556021673f1d1be3517501c9b5c157b070e
Eddie Moon <eddiemoonie@gmail.com>
2018-03-02 00:51:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name :song
 
   @@song_count = 0
 
@@ -10,7 +10,7 @@ class Artist
   end
 
   def add_song(song)
-  self.songs << song
+    self.songs << song
     song.artist = self
     @@song_count += 1
   end
