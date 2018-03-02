0eed1f2e87e009d850af0942e68a703502235daf
Eddie Moon <eddiemoonie@gmail.com>
2018-03-02 00:55:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,9 +9,9 @@ class Artist
     @songs = []
   end
 
-  def add_song(song)
-    self.songs << song
-    song.artist = self
+  def add_song(name)
+    self.songs << name
+    name.artist = self
     @@song_count += 1
   end
 
