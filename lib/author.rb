a75407c5782187126868970d42c9cc81ee8e5b9b
Eddie Moon <eddiemoonie@gmail.com>
2018-03-02 01:18:43 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -20,7 +20,7 @@ class Author
   end
 
   def add_post_by_title(title)
-    post = Author.new(title)
+    post = Post.new(title)
     self.posts << post
     post.author = self
     @@post_count += 1
